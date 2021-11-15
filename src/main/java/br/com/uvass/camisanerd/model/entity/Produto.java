8a47c9a752b61c4d545aa73ce3f5e34fb7c453a9
AntonioTadeu <antoniotadeubf98@gmail.com>
2021-11-15 17:54:13 -0300

    Feat: Criação de Classe
    Foram feitas as Classes.

---
+++ src/main/java/br/com/uvass/camisanerd/model/entity/Produto.java
@@ -0,0 +1,74 @@
+package br.com.uvass.camisanerd.model.entity;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+public class Produto {
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private Float preco;
+    private String fotoUrl;
+    private LocalDate dataCadastro;
+    private Timestamp dataUltimaAtualizacao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+
+    public String getFotoUrl() {
+        return fotoUrl;
+    }
+
+    public void setFotoUrl(String fotoUrl) {
+        this.fotoUrl = fotoUrl;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Timestamp getDataUltimaAtualizacao() {
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
+        return timestamp;
+    }
+
+    public void setDataUltimaAtualizacao(Timestamp dataUltimaAtualizacao) {
+        this.dataUltimaAtualizacao = dataUltimaAtualizacao;
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE produto(
                         id                     INT NOT NULL,
                         nome                   VARCHAR(120) NOT NULL,
                         descricao              VARCHAR(200) NOT NULL,
-                        preco                  FLOAT NOT NULL
+                        preco                  FLOAT NOT NULL,
                         fotoUrl                VARCHAR(600),
                         dataCadastro           DATE NOT NULL,
                         dataUltimaAtualizacao  TIMESTAMP NOT NULL,
