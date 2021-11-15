da7861a01d7c7cf51b75ea8beed11e29587261c9
AntonioTadeu <antoniotadeubf98@gmail.com>
2021-11-15 17:18:09 -0300

    Feat: Primeiras Configurações
    Foram feitas as primeiras configurações.

---
+++ src/main/java/br/com/uvass/camisanerd/CamisanerdApplication.java
@@ -0,0 +1,13 @@
+package br.com.uvass.camisanerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamisanerdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamisanerdApplication.class, args);
+    }
+
+}
